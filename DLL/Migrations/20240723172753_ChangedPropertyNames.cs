e9c215f01542eecdfd07f44501d9b1c4936f7747
giorgi <bazadzegiorgi9@gmail.com>
2024-07-23 23:19:05 +0400

    Added rightConfiguration to WordRoomConfiguration.cs

---
+++ DLL/Migrations/20240723172753_ChangedPropertyNames.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DLL.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangedPropertyNames : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsGuessed",
+                table: "WordRooms",
+                newName: "IsUncovered");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsUncovered",
+                table: "WordRooms",
+                newName: "IsGuessed");
+        }
+    }
+}
